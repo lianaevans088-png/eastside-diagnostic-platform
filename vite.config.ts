064271cf9a4f393282f5a740e3ca1964330a83d8
lianaevans088-png <lianaevans088@gmail.com>
2026-05-09 00:58:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/eastside-diagnostic-platform/',
   plugins: [react()],
-  server: { host: '0.0.0.0', port: 5173 }
 });
